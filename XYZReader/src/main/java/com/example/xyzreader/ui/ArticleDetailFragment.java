96ff64f70c2d7893b085da7133f6f261fd28cac9
DELL <raghvendrasrivastava14@gmail.com>
2017-01-19 07:54:10 +0530

    fix image fading in CollapsingToolbar

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -42,24 +42,17 @@ public class ArticleDetailFragment extends Fragment implements
     private static final String TAG = "ArticleDetailFragment";
 
     public static final String ARG_ITEM_ID = "item_id";
-    private static final float PARALLAX_FACTOR = 1.25f;
 
     private Cursor mCursor;
     private long mItemId;
     private View mRootView;
     private int mMutedColor = 0xFF333333;
     private NestedScrollView mScrollView;
-    private DrawInsetsFrameLayout mDrawInsetsFrameLayout;
-    private ColorDrawable mStatusBarColorDrawable;
 
     private CollapsingToolbarLayout mCollapsingToolbar;
     private AppBarLayout mAppBar;
-    private int mTopInset;
-    private View mPhotoContainerView;
     private ThreeTwoImageView mPhotoView;
-    private int mScrollY;
     private boolean mIsCard = false;
-    private int mStatusBarFullOpacityBottom;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -85,14 +78,10 @@ public class ArticleDetailFragment extends Fragment implements
         }
 
         mIsCard = getResources().getBoolean(R.bool.detail_is_card);
-//        mStatusBarFullOpacityBottom = getResources().getDimensionPixelSize(
-//                R.dimen.detail_card_top_margin);
+
         setHasOptionsMenu(true);
     }
-//
-//    public ArticleDetailActivity getActivityCast() {
-//        return (ArticleDetailActivity) getActivity();
-//    }
+
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -109,19 +98,16 @@ public class ArticleDetailFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
-//        mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-//                mRootView.findViewById(R.id.draw_insets_frame_layout);
-//        mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
-//            @Override
-//            public void onInsetsChanged(Rect insets) {
-//                mTopInset = insets.top;
-//            }
-//        });
+
 
         mCollapsingToolbar = (CollapsingToolbarLayout) mRootView.findViewById(R.id.collapsing_toolbar);
         mAppBar = (AppBarLayout) mRootView.findViewById(R.id.appbar);
         Toolbar toolbar = (Toolbar) mRootView.findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back, null));
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
+            toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back, null));
+        } else {
+            toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back));
+        }
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -146,28 +132,7 @@ public class ArticleDetailFragment extends Fragment implements
         });
 
         mScrollView = (NestedScrollView) mRootView.findViewById(R.id.scrollview);
-//        mScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-//            @Override
-//            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-//                mScrollY = scrollY;
-//                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-////                updateStatusBar();
-//            }
-//        });
-//        mScrollView.setCallbacks(new ObservableScrollView.Callbacks() {
-//            @Override
-//            public void onScrollChanged() {
-//                mScrollY = mScrollView.getScrollY();
-//                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-////                mPhotoContainerView.setTranslationY((int) (mScrollY - mScrollY / PARALLAX_FACTOR));
-//                updateStatusBar();
-//            }
-//        });
 
-
-//        mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
-
-//        mStatusBarColorDrawable = new ColorDrawable(0);
 
         mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -186,55 +151,14 @@ public class ArticleDetailFragment extends Fragment implements
         });
 
         bindViews();
-//        updateStatusBar();
+
         return mRootView;
     }
 
     @Override
     public void onResume() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Slide slide = new Slide(Gravity.BOTTOM);
-//            slide.excludeTarget(android.R.id.statusBarBackground, true);
-//            slide.excludeTarget(android.R.id.navigationBarBackground, true);
-//            slide.excludeTarget(R.id.appbar, true);
-//            slide.setInterpolator(
-//                    AnimationUtils.loadInterpolator(getActivity(),
-//                            android.R.interpolator.linear_out_slow_in));
-//            slide.setDuration(getResources().getInteger(android.R.integer.config_longAnimTime));
-//            getActivity().getWindow().setEnterTransition(slide);
-//        }
         super.onResume();
     }
-
-    //
-//    private void updateStatusBar() {
-//        int color = 0;
-//        if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
-//            float f = progress(mScrollY,
-//                    mStatusBarFullOpacityBottom - mTopInset * 3,
-//                    mStatusBarFullOpacityBottom - mTopInset);
-//            color = Color.argb((int) (255 * f),
-//                    (int) (Color.red(mMutedColor) * 0.9),
-//                    (int) (Color.green(mMutedColor) * 0.9),
-//                    (int) (Color.blue(mMutedColor) * 0.9));
-//        }
-//        mStatusBarColorDrawable.setColor(color);
-//        mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-//    }
-//
-//    static float progress(float v, float min, float max) {
-//        return constrain((v - min) / (max - min), 0, 1);
-//    }
-//
-//    static float constrain(float val, float min, float max) {
-//        if (val < min) {
-//            return min;
-//        } else if (val > max) {
-//            return max;
-//        } else {
-//            return val;
-//        }
-//    }
 
     private void bindViews() {
         if (mRootView == null) {
@@ -245,7 +169,6 @@ public class ArticleDetailFragment extends Fragment implements
         TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
         bylineView.setMovementMethod(new LinkMovementMethod());
         TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
-//        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
@@ -275,7 +198,6 @@ public class ArticleDetailFragment extends Fragment implements
                                 mRootView.findViewById(R.id.article_byline)
                                         .setBackgroundColor(mMutedColor);
 
-//                                updateStatusBar();
                             }
                         }
 
@@ -322,15 +244,5 @@ public class ArticleDetailFragment extends Fragment implements
         bindViews();
     }
 
-//    public int getUpButtonFloor() {
-////        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
-////            return Integer.MAX_VALUE;
-////        }
-////
-////        // account for parallax
-////        return mIsCard
-////                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-////          : mPhotoView.getHeight() - mScrollY;
-//        return 1;
-//    }
+
 }
